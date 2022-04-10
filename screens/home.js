24a63e05ef891b560c45a559ca87413b88a93212
ant-biraj <biraj.gupta@antcreatives.com>
2022-04-10 12:41:06 +0530

    add dummy data

--- screens/home.js
+++ screens/home.js
@@ -1,5 +1,5 @@
-import React from "react"
-import { StyleSheet, View, Text, Button } from "react-native"
+import React, {useState} from "react"
+import { StyleSheet, View, Text, Button, FlatList, TouchableOpacity } from "react-native"
 import {GlobalStyles} from "../styles/global"
 
 
@@ -8,6 +8,11 @@ export default function Home({navigation}) {
     // const pressHandler = () => {
     //     navigation.push('ReviewDetails')
     // }
+    const [reviews, setReviews] = useState([
+        {title:"Zelda, Breath of Fresh Air", rating:5, body:"lorem ipsum", key:1},
+        {title:"Gotta Catch them all (again)", rating:4, body:"lorem ipsum", key:2},
+        {title:"Not So, 'Final' Fantasy", rating:3, body:"lorem ipsum", key:3},
+    ])
 
     return (
         <View style={GlobalStyles.container}>
